041b5b77c1070a91dc9dd2ac459c7f752c91488c
adamGrgic <argrgic@gmail.com>
2025-10-07 22:13:10 -0600

    add other CRUD tests

--- src/db.rs
+++ src/db.rs
@@ -66,6 +66,7 @@ pub fn get_all_todos(conn: &Connection) -> Result<Vec<Todo>> {
     Ok(todos)
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -94,4 +95,104 @@ mod tests {
         let row = rows.next().unwrap();
         assert!(row.is_some());
     }
+
+    #[test]
+    fn test_update_todo() {
+        let mut conn = Connection::open_in_memory().unwrap();
+        let migrations: rusqlite_migration::Migrations =
+            MigrationsBuilder::from_directory(&MIGRATION_DIR)
+                .unwrap()
+                .finalize();
+        migrations.to_latest(&mut conn).unwrap();
+
+        let add_todo_test = AddTodo {
+            title: "test title".to_string(),
+            description: "test description".to_string(),
+        };
+
+        let result = add_todo(&conn, &add_todo_test);
+        assert!(result.is_ok());
+
+        let mut stmt = conn
+            .prepare("SELECT title, description FROM todos WHERE title='test title'")
+            .unwrap();
+        let mut rows = stmt.query([]).unwrap();
+        let row = rows.next().unwrap();
+        assert!(row.is_some());
+
+        let update_todo_test = UpdateTodo {
+            id: 1,
+            title: "I am a new test title".to_string(),
+            description: "test description".to_string(),
+        };
+
+        let updated_result = update_todo(&conn, &update_todo_test);
+        assert!(updated_result.is_ok());
+
+        let mut stmt = conn
+            .prepare("SELECT title, description FROM todos WHERE title='I am a new test title'")
+            .unwrap();
+        let mut rows = stmt.query([]).unwrap();
+        let row = rows.next().unwrap();
+        assert!(row.is_some());
+
+    }
+
+    #[test]
+        fn test_remove_todo() {
+        let mut conn = Connection::open_in_memory().unwrap();
+        let migrations: rusqlite_migration::Migrations =
+            MigrationsBuilder::from_directory(&MIGRATION_DIR)
+                .unwrap()
+                .finalize();
+        migrations.to_latest(&mut conn).unwrap();
+
+        let add_todo_test = AddTodo {
+            title: "test title".to_string(),
+            description: "test description".to_string(),
+        };
+
+        let result = add_todo(&conn, &add_todo_test);
+        assert!(result.is_ok());
+
+        let mut stmt = conn
+            .prepare("SELECT title, description FROM todos WHERE title='test title'")
+            .unwrap();
+        let mut rows = stmt.query([]).unwrap();
+        let row = rows.next().unwrap();
+        assert!(row.is_some());
+
+        let update_todo_test = UpdateTodo {
+            id: 1,
+            title: "I am a new test title".to_string(),
+            description: "test description".to_string(),
+        };
+
+        let updated_result = update_todo(&conn, &update_todo_test);
+        assert!(updated_result.is_ok());
+
+        let mut stmt = conn
+            .prepare("SELECT title, description FROM todos WHERE title='I am a new test title'")
+            .unwrap();
+        let mut rows = stmt.query([]).unwrap();
+        let row = rows.next().unwrap();
+        assert!(row.is_some());
+
+        let delete_todo_test = RemoveTodo {
+            id: 1,
+        };
+
+        let delete_result = remove_todo(&conn, &delete_todo_test);
+        assert!(delete_result.is_ok());
+
+        let mut stmt = conn
+            .prepare("SELECT title, description FROM todos WHERE id=1")
+            .unwrap();
+        let mut rows = stmt.query([]).unwrap();
+        let row = rows.next().unwrap();
+        assert!(!row.is_some());
+
+    }
+
 }
+
